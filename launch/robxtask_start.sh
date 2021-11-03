add1fecf1d580a4dba1ec99ef57b1c601aa04efd
nerovalerius <crypto@mailbox.org>
2021-11-03 14:24:01 +0100

    minor changes to start launch file - now does not require passkey anymore

--- launch/robxtask_start.sh
+++ launch/robxtask_start.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 
-# start the ROS action server in python using rospy
-gnome-terminal -- rosrun rxt_skills_chasi chasi_action_server.py
-sleep 5
-
-
 # start raspberry pi connection
-gnome-terminal -- sshpass -p "ubuntu" ssh ubuntu@192.168.5.3 "roslaunch arti_chasi_mark3 arti_chasi_mark3_upstart_with_teleop.launch"
+#gnome-terminal -- sshpass -p "ubuntu" ssh ubuntu@192.168.5.3 "roslaunch arti_chasi_mark3 arti_chasi_mark3_upstart_with_teleop.launch"
+gnome-terminal -- ssh ubuntu@192.168.5.3 "roslaunch arti_chasi_mark3 arti_chasi_mark3_upstart_with_teleop.launch"
 sleep 50
 
 
 # start the rviz tool with loaded map and properties
 gnome-terminal -- roslaunch rxt_skills_chasi Chasi.launch
+sleep 5
+
+
+# start the ROS action server in python using rospy
+gnome-terminal -- rosrun rxt_skills_chasi chasi_action_server.py
+sleep 5
 
 
 
